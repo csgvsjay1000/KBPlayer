a12e00d95a874ce248b2a412c833897b2ee229b5
kobi_2016 <254633397@qq.com>
2016-05-18 15:20:07 +0800

    tt

---
+++ KBPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBPlayer
+//
+//  Created by chengshenggen on 5/18/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
